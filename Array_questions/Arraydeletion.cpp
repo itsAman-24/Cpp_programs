897214a463192aa61b872236aaec3d4835dd69d1
itsAman-24 <amanpan2410@gmail.com>
2023-08-26 12:43:27 +0530

    added

---
+++ Array_questions/Arraydeletion.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    int num = 10;
+    cout<<"Enter the elements in the array"<<endl;
+    for(int i = 0; i <= num; i++)
+    { 
+        cin>>arr[i];
+    }
+    
+    int element;
+    cout<<"Enter the element that you want to delete from the array"<<endl;
+    cin>>element;
+    int found = 0;
+    
+    for(int i = 0; i < num; i++)
+    {
+        if(arr[i] == element)
+        {
+            for(int j = i; j < (num-1); j++)
+            {
+                arr[j] = arr[j+1];
+                found = 1;
+                i--;
+                num--;
+
+            }
+        }
+    }
+
+    if(found == 0){
+        cout<<"Element not found"<<endl;
+    }
+    else{
+        cout<<"Element deleted successfully"<<endl;
+    
+
+    for(int i = 0; i < num; i++)
+    {
+        cout<<arr[i]<<"  ";
+
+    }
+    }
+
+   return 0;   
+}
